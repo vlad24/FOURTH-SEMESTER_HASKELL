6a27a7e265e554188265a8edad7d510ff7ebc171
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2014-02-28 14:02:21 +0400

    DONE COMMIT.2.2. powers of two

---
+++ HW_2/twoPowers/twoPowers.hs
@@ -0,0 +1,15 @@
+--Inserting into head of a list - optimization, better than inserting into a tail
+--div 2^n 2  - optimization, considering that div k 2 is a bit shift
+generateTwoPowers :: Int -> [Int]
+generateTwoPowers n = powerChopper [] (2^n)
+    where
+    powerChopper :: [Int] -> Int -> [Int]
+    powerChopper list 0 = list
+    powerChopper list twoPower = powerChopper (twoPower:list) (div twoPower 2)
+--
+main :: IO ()
+main = do
+    putStr("Enter the length of your future list of powers of two : ")
+    n <- readLn
+    putStr("Your list: ")
+    print(generateTwoPowers(n))
