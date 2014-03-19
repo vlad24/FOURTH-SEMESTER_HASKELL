1756db7d71a39ac177b2ed3d653f5d477f2c269d
Vladislav <vlad.pavlov24@gmail.com>
2014-03-19 16:23:46 +0400

    DONE COMMIT. 3.6. brackets are rewtitten using stacks

--- HW_3/brackets.hs
+++ HW_3/brackets.hs
@@ -1,13 +1,25 @@
---accumulator usage
+isClosingPair :: Char -> Char -> Bool
+isClosingPair '(' ')' = True
+isClosingPair '[' ']' = True
+isClosingPair '{' '}' = True
+isClosingPair _ _ = False
+
+isOpen :: Char -> Bool
+isOpen x  = elem x  ['(', '[', '{']
+
+isClose :: Char -> Bool
+isClose x = elem x [')', ']', '}']
+
 validate :: String -> Bool
-validate (x : xs)  = bracketsOK (x:xs) 0 where
-	bracketsOK :: String -> Int -> Bool
-	bracketsOK [] balance = (balance == 0)
-	bracketsOK (x : xs) balance | ( x == '(' )        =  if (balance + 1 >= 0) then  bracketsOK (xs) (balance + 1)
-						 else False
-			            | ( x == ')' )        =  if (balance - 1 >= 0) then  bracketsOK (xs) (balance - 1)
-						 else False
-			            | otherwise         =  bracketsOK (xs) balance
+validate (x : xs)  = bracketsOK (x:xs) [] where
+	bracketsOK :: String->[Char]->Bool
+	bracketsOK [] [] = True
+	bracketsOK [] stack = False
+	bracketsOK (x:xs) stack | (isOpen x) = bracketsOK xs (x:stack)
+			       | (isClose x) = if (isClosingPair (head stack) x) 
+					     then bracketsOK xs (tail stack)
+				                  else False
+			       | otherwise = bracketsOK xs stack
 main = do
 	putStrLn("Enter your string please and we will check the right sequence of brackets there: ")
 	putStr("Your string : ")
