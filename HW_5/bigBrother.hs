1d1b1b0de2d111fc842bcc2405ca57f941a2884e
Vladislav <vlad.pavlov24@gmail.com>
2014-05-04 09:11:58 +0400

    DONE COMMIT. bigBrother. monads bind is now used

--- HW_5/bigBrother.hs
+++ HW_5/bigBrother.hs
@@ -2,7 +2,6 @@ bigBrother :: [Int] -> IO ()
 bigBrother [] = print("No Big brother")
 bigBrother (z:[]) = print("No Big brother")
 bigBrother (y:z:[]) = print ("No Big brother")
-bigBrother (x:y:z:zs) = if ((y > x) && (y > z)) then
-                                    print("Big brother : " ++ show y)
-                                else
-                                    bigBrother (y:z:zs)
+bigBrother (x:y:z:zs) = return y >>= \v -> if ((x < v) && (z < v)) then
+                                                              print(show y)
+                                                            else bigBrother (y:z:zs)
